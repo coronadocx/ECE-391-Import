6ef1d758b287e0933ef536d13dc538fb2151c0b5
kgoenka2 <kgoenka2@illinois.edu>
2019-03-08 05:49:47 -0600

    rtc files added

---
+++ student-distrib/rtc.c
@@ -0,0 +1,9 @@
+/* c file for rtc */
+#include "lib.h"
+#include "i8259.h"
+
+void init_rtc(){
+
+
+  return;
+}
---
+++ student-distrib/rtc.h
@@ -0,0 +1,8 @@
+/* header file for keyboard */
+#ifndef RTC_H
+#define RTC_H
+
+
+void init_rtc();
+
+#endif

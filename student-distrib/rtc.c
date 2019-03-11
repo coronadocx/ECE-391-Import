1703699e8a739b911f27675dcd3d62f3e3242489
kgoenka2 <kgoenka2@illinois.edu>
2019-03-11 13:36:49 -0500

    Merge branch 'master' of https://gitlab.engr.illinois.edu/ece391_sp19/mp3_group_10 into kushal
    Merged into kushaldev branch after comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -3,6 +3,18 @@
 #include "i8259.h"
 #include "rtc.h"
 
+/*
+ * init_rtc
+ *   DESCRIPTION: Function to initialize the RTC, including setting the correct
+ *                Registers A and B and setting the frame rate
+ *   INPUTS: None
+ *   OUTPUTS:None
+ *   RETURN VALUE: None
+ *   SIDE EFFECTS:  Initializes the RTC, and sets the current frequency
+ *                  Given by the lower 4 bits of register A
+ */
+
+
 void init_rtc(){
 /* referenced wiki.osdev.org/RTC to understand the rtc */
 outb(REG_B,RTC_CMD_PORT);

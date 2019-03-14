e0ac0126f0a6c144f728b2d9a5a2afb4b3cda290
Chris Coronado <ccoron7@illinois.edu>
2019-03-13 22:22:37 -0500

    RTC for CP2 is almost done, just need to finish rtc_read(). Note, rtc_read() currently breaks on purpose.

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -13,8 +13,6 @@
  *   SIDE EFFECTS:  Initializes the RTC, and sets the current frequency
  *                  Given by the lower 4 bits of register A
  */
-
-
 void init_rtc(){
 /* referenced wiki.osdev.org/RTC to understand the rtc */
 outb(REG_B,RTC_CMD_PORT);
@@ -24,3 +22,107 @@ outb(prev|RTC_MASK,RTC_DATA_PORT);
 enable_irq(RTC_IRQ_NO);
   return;
 }
+
+
+/* 
+ * 	rtc_read
+ *	DESCRIPTION: Blocks current process until next RTC interrupt
+ * 	INPUT:	fd		- RTC file descriptor
+ * 	OUTPUT:	buf		- UNUSED
+ *			nbytes	- UNUSED
+ * 	RETURN VALUE: Returns 0 once an interrupt has occurred	
+ * 	SIDE EFFECT: NONE
+ *  NOTE: This does not read the current RTC frequency
+ */
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes)
+{
+	volatile int32_t rtc_interrupt_occurred;
+
+	// Do something // Set a flag
+
+	while  (!rtc_interrupt_occurred) { 	// Wait for RTC interrupt
+		// Do something
+	}
+
+	*(int*)(0)++;		//FIXME forced pagefault to make sure this breaks
+	return 0;
+}
+
+
+/*
+ * 	rtc_write
+ * 	DESCRIPTION: Changes the RTC's frequency
+ * 	INPUT:	fd		- File descriptor for the RTC
+ * 			buf		- Pointer to a 4-byte integer specifying the interrupt rate
+ *	OUTPUT: nbytes  - Holds the number of bytes written (always 4 on success)
+ *	RETURN VALUE: 0 on success, -1 on failure
+ *	SIDE EFFECT: Modifies the RTC
+ */
+int32_t rtc_write(int32_t fd,const void * buf, int32_t nbytes)
+{
+	int32_t tmp;
+	int8_t rate = 6;
+	int8_t prev;
+	//Make sure input is valid
+	if ( buf == NULL 		||
+		*buf & ((*buf)-1) 	||
+		*buf > MAX_FREQ) {
+		return -1;
+	}
+
+	// Calculate the RTC rate setting
+	tmp = MAX_FREQ;
+	while (tmp != *buf){
+		rate++;
+		tmp = tmp >> 1; 
+	}
+
+	// Change RTC to calculated rate
+	cli();										// Disable interrupts
+	outb(RTC_CMD_PORT, REG_A_NMI_OFF);			// Select A, NMI off
+	prev = inb(RTC_DATA_PORT);					// Collect old state
+	outb(RTC_CMD_PORT, REG_A_NMI_OFF);			// Select A again
+	outb(RTC_DATA_PORT, (prev & 0xF0) | rate);	// Write new rate to RTC
+	sti();										// Enable interrupts
+
+	return 0;
+}
+
+
+/*
+ * 	rtc_open
+ * 	DESCRIPTION: Sets RTC frequency to 2Hz then returns
+ * 	INPUT: filename - UNUSED
+ *	OUTPUT: none
+ *	RETURN VALUE: 0 always 
+ *	SIDE EFFECT: Modities the 
+ */
+int32_t rtc_open(const uint8_t * filename)
+{
+	//Set the RTC to 2Hz
+	*(int*)(0)++;		//FIXME forced pagefault to make sure this breaks 
+	return 0;
+}
+
+
+/*	NOTE: Will be implemented in later checkpoint
+ * 	rtc_close
+ *  DESCRIPTION: Does nothing
+ * 	INPUT: none
+ *	OUTPUT:none
+ *	RETURN VALUE: 0 always
+ *	SIDE EFFECT: none
+ */
+int32_t rtc_close(int32_t fd)
+{
+	return 0;
+}
+
+
+/*
+	For CP 5 -- How to virturalize RTC:
+	Set RTC to highest frequency
+	Have a handful of counters for a number of different RTCs
+	each interrupt, increment the counter
+	when a counter reaches a certain value (which emulates lower frequency), reset and raise a new interrupt
+*/

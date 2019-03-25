35c631a9a96dd77c9eed4c4405b5ed9749e92987
Khanna <rohank2@illinois.edu>
2019-03-25 17:06:36 -0500

    final commit

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -419,7 +419,7 @@ void launch_tests(unsigned int start ){
 
 
 	 // TEST CASE 1 LIST ALL FILES IN DIRECTORY
-	TEST_OUTPUT("dir_read_test", dir_read_test());
+	//TEST_OUTPUT("dir_read_test", dir_read_test());
 
 	 // TEST CASE 2 READ FROM FILES
 	// TEST_OUTPUT("read_data_fromfile", read_data_fromfile("frame0.txt"));

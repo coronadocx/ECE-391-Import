8ad2b6d3cf7bfbf784185b0cd40e63feafe017a4
Chris Coronado <ccoron7@illinois.edu>
2019-04-08 14:25:36 -0500

    Commented and removed magic numbers in pcb, terminal, and filesystem

--- student-distrib/pcb.h
+++ student-distrib/pcb.h
@@ -1,32 +1,23 @@
 #ifndef PCB_H
 #define PCB_H
 
-
-
 #include "types.h"
 #include "rtc.h"
 #include "filesystem.h"
 #include "terminal.h"
 
-
-
-
 #define PCB_MASK    0xFFE000
 #define FD_ARR_SIZE       8
 
-/* Function to get the address of the current pcb */
-
+/* Operation Jumptable Struct */
 typedef struct operation_table{
  int32_t (*open)(int8_t* filename);
  int32_t (*read)(int32_t fd,void* buf, int32_t nbytes);
  int32_t (*write)(int32_t fd,const void* buf, int32_t nbytes);
  int32_t (*close)(int32_t fd);
-
 }otable_t;
 
-
 /* struct for a file descriptor */
-
 typedef struct file_descriptor{
   void** operationstable;
   int32_t inode_num;
@@ -34,10 +25,7 @@ typedef struct file_descriptor{
   uint8_t flags[4]; // Define file type
 }file_desc;
 
-
-
-
-
+/* PCB struct decleration*/
 typedef struct task_struct{
   file_desc fd_array[FD_ARR_SIZE];
   struct task_struct* parent;
@@ -45,11 +33,12 @@ typedef struct task_struct{
   int32_t parent_esp;
   int32_t parent_ebp;
   int32_t parent_process_id;
-
 }pcb;
+
+/* Function to get the address of the current pcb */
  pcb* get_pcb_address();
 
-
-
-
 #endif
+
+
+

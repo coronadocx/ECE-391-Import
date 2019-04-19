10c4f5de88a168dfb1ec9adfe581c8a8b500a643
kgoenka2 <kgoenka2@illinois.edu>
2019-04-19 15:49:08 -0500

    Multiple terminals kushal

---
+++ student-distrib/pit.c
@@ -0,0 +1,21 @@
+#include "rtc.h"
+
+
+
+// void init_pit(){
+// /* referenced wiki.osdev.org/RTC to understand the rtc */
+// outb(REG_B,RTC_CMD_PORT);
+// char prev = inb(RTC_DATA_PORT);
+// outb(REG_B,RTC_CMD_PORT);
+// outb(prev|RTC_MASK,RTC_DATA_PORT);
+// enable_irq(RTC_IRQ_NO);
+//   return;
+// }
+//
+//
+// int32_t pit_write(int32_t fd,const void * buf, int32_t nbytes){
+//
+//
+//
+//
+// }
